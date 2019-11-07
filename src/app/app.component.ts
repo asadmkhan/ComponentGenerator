ccaa93b5d7cd92e8c6df87ba41f104a1849956f2
Asad Khan <asadmateenkhan@gmail.com>
2019-11-07 17:53:12 +0400

    update 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,79 +1,109 @@
-import {Compiler, Component, Injector, NgModule, NgModuleRef, ViewChild, ViewContainerRef,OnInit, Type,ComponentFactoryResolver} from "@angular/core";
-import {FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { AppMaterialModule } from './material-module';
-
+import {
+  Compiler,
+  Component,
+  Injector,
+  NgModule,
+  NgModuleRef,
+  ViewChild,
+  ViewContainerRef,
+  OnInit,
+  Type,
+  ComponentFactoryResolver
+} from "@angular/core";
+import { FormBuilder, FormGroup, NgForm, Validators } from "@angular/forms";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { AppMaterialModule } from "./material-module";
+import { validator } from "html-validator";
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-
-
-export class AppComponent implements OnInit  {
-  
+export class AppComponent implements OnInit {
+  componentRef = null;
   componentGeneratorForm: FormGroup;
-  componentText: string = '';
-  componentTemplateCode : string = '';
+  componentText: string = "";
+  componentTemplateCode: string = "";
 
-  @ViewChild('vc', {static: true, read: ViewContainerRef}) _container: ViewContainerRef;
-  constructor(private _compiler: Compiler,
-              private _injector: Injector,
-              private _m: NgModuleRef<any>,
-              private formBuilder: FormBuilder,
-              private resolver: ComponentFactoryResolver) {
-  }
+  @ViewChild("vc", { static: true, read: ViewContainerRef })
+  _container: ViewContainerRef;
+  constructor(
+    private _compiler: Compiler,
+    private _injector: Injector,
+    private _m: NgModuleRef<any>,
+    private formBuilder: FormBuilder,
+    private resolver: ComponentFactoryResolver
+  ) {}
 
   ngOnInit() {
-  
     this.componentGeneratorForm = this.formBuilder.group({
-     
-      'componentText': [null, Validators.required],
-      'componentTemplateCode': [null, Validators.required],
-      
+      componentText: [null, Validators.required],
+      componentTemplateCode: [null, Validators.required]
     });
-   
   }
 
   onFormSubmit(form: NgForm) {
-  
-    this.CreateComponent();
+    var markupToCheck = this.componentTemplateCode.replace("{{Testtext}}", "");
+    var isValidMarkup = this.isValidMarkup(markupToCheck);
+    var doesExpressionExists =
+      this.componentTemplateCode.indexOf("Testtext") >= 0;
+
+    if (doesExpressionExists && isValidMarkup) {
+      this.CreateComponent();
+    } else if (!doesExpressionExists) {
+      alert('Please use "Testtext" as binding property in template code');
+    } else if (!isValidMarkup) {
+      alert("HTML is not valid");
+    }
+  }
+
+  updateComponentValue() {
+    if (this.componentRef) {
+      this.componentRef.instance.Updater(this.componentText);
+    }
   }
 
   CreateComponent() {
+    const template =
+      "<div  fxLayout='row'  fxLayout.xs='column' fxLayoutGap='1%' fxLayoutAlign='center center'>" +
+      "<div fxFlex='50%'>" +
+      " <div fxLayout='row' fxLayoutAlign='center center'> " +
+      "<label class='cardTitle'>Component B " +
+      " </label>" +
+      " </div> " +
+      " <div fxLayout='row' fxLayoutAlign='center center'> " +
+      this.componentTemplateCode +
+      " </div> " +
+      "</div>" +
+      "</div>";
 
-    
-    const template = "<div  fxLayout='row'  fxLayout.xs='column' fxLayoutGap='1%' fxLayoutAlign='center center'>" +
-                      "<div fxFlex='50%'>" +
-                      " <div fxLayout='row' fxLayoutAlign='center center'> " +
-                      "<label class='cardTitle'>Component B " +
-                      " </label>" +
-                      " </div> "+
-                      " <div fxLayout='row' fxLayoutAlign='center center'> " +
-                      this.componentTemplateCode + 
-                     
-                      " </div> "+
-                     
-                      "</div>" +
-                      "</div>";
+    const tmpCmp = Component({ template: template })(
+      class {
+        Testtext: string = "My New Value";
+      }
+    );
+    const tmpModule = NgModule({
+      declarations: [tmpCmp],
+      imports: [FlexLayoutModule]
+    })(class {});
 
-                      
-      const tmpCmp = Component({template: template})(class {
+    this._container.clear();
+
+    this._compiler
+      .compileModuleAndAllComponentsAsync(tmpModule)
+      .then(factories => {
+        const f = factories.componentFactories[0];
+        this.componentRef = this._container.createComponent(f);
+        this.componentRef.instance.Testtext = this.componentText;
+        this.componentRef.instance.Updater = function(updatedValue) {
+          this.Testtext = updatedValue;
+        };
+        this._container.insert(this.componentRef.hostView);
       });
-      const tmpModule = NgModule({declarations: [tmpCmp],imports: [FlexLayoutModule]} )(class {
-      });
-
-      this._container.clear();
-      
-      this._compiler.compileModuleAndAllComponentsAsync(tmpModule)
-          .then((factories) => {
-              const f = factories.componentFactories[0];
-              const cmpRef = f.create(this._injector, [], null, this._m);
-              cmpRef.instance.Testtext = this.componentText;
-              this._container.insert(cmpRef.hostView);
-          })
   }
 
-  
- 
+  isValidMarkup(tag) {
+    var isvalid = /<([A-Za-z][A-Za-z0-9]*)\b[^>]*>(.*?)<\/\1>/.test(tag);
+    return isvalid;
+  }
 }
